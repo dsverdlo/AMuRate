5f2f9b357acbc6becd5427be913250bec72f8d0f
David Sverdlov <dsverdlo@vub.ac.be>
2012-11-23 22:05:21 +0100

    Upload Logboek
    + GUI probeersel

--- Repository/gen/com/dsverdlo/bachelorproef/R.java
+++ Repository/gen/com/dsverdlo/bachelorproef/R.java
@@ -13,8 +13,19 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int button_cancel=0x7f060002;
+        public static final int button_submit=0x7f060001;
+        public static final int tex=0x7f060000;
+    }
+    public static final class layout {
+        public static final int main_test=0x7f030000;
+    }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int app_name=0x7f040000;
+        public static final int cancel=0x7f040003;
+        public static final int search=0x7f040001;
+        public static final int submit=0x7f040002;
     }
     public static final class style {
         /** 
@@ -38,10 +49,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f040000;
+        public static final int AppBaseTheme=0x7f050000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f040001;
+        public static final int AppTheme=0x7f050001;
     }
 }
--- Repository/src/com/dsverdlo/bachelorproef/MainActivity.java
+++ Repository/src/com/dsverdlo/bachelorproef/MainActivity.java
@@ -1,29 +1,12 @@
 package com.dsverdlo.bachelorproef;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.StringWriter;
-import java.io.Writer;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.conn.ConnectTimeoutException;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpConnectionParams;
-import org.apache.http.params.HttpParams;
-import org.w3c.dom.Text;
-
 import android.R;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.app.Activity;
-import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -34,7 +17,6 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 
 		System.out.println("0.5");
-		setContentView(R.layout.activity_list_item);
 		Toast t = Toast.makeText(getApplicationContext(), "Main created!", Toast.LENGTH_LONG);
 		t.show();
 		  
@@ -42,72 +24,29 @@ public class MainActivity extends Activity {
 		MyConnection myc = new MyConnection();
 		myc.getTest(this.getApplicationContext());
 		
+		TextView tex = new TextView(this.getApplicationContext());
 		
+		tex.setText("Enter an artist name");
 		
-		/*
-		try {
-
-			String yeah = convertStreamToString(getInputStreamFromUrl("http://wilma.vub.ac.be/~dsverdlo/bachproef/api.php", 30000));
-			Log.d(VIBRATOR_SERVICE, yeah);
-			System.out.println(yeah);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			Log.d(VIBRATOR_SERVICE, "failed");
-			e.printStackTrace();
-		}
-		Log.d(VIBRATOR_SERVICE, "done");
-		System.out.println("donee");
-	}
-
-	public static InputStream getInputStreamFromUrl(String url, int timeoutInMilliseconds) {
-		InputStream content = null;
-		try {
-			// set up GET request
-			HttpGet httpGet = new HttpGet(url);
-
-			// set connection timeout + create client
-			HttpParams httpParams = new BasicHttpParams();
-			HttpConnectionParams.setConnectionTimeout(httpParams, timeoutInMilliseconds);
-			HttpClient httpClient = new DefaultHttpClient(httpParams);
-			Log.d(VIBRATOR_SERVICE, "a1");
-			// execute GET request
-			HttpResponse httpResponse = httpClient.execute(httpGet);
-			Log.d(VIBRATOR_SERVICE, "a2");
-			content = httpResponse.getEntity().getContent();
-			Log.d(VIBRATOR_SERVICE, "a3");
-		} catch (ConnectTimeoutException e) {
-			// handle the timeout exception !
-		} catch (Exception e) {
-			// other exceptions
-		}
-		return content;
-	}
-
-	public static String convertStreamToString(InputStream is) throws IOException {
-		//
-		// To convert the InputStream to String we use the
-		// Reader.read(char[] buffer) method. We iterate until the
-		// Reader return -1 which means there's no more data to
-		// read. We use the StringWriter class to produce the string.
-		//
-		if (is != null) {
-			Writer writer = new StringWriter();
-			char[] buffer = new char[1024];
-			try {
-				Reader reader = new BufferedReader(new InputStreamReader(is,
-						"UTF-8"));
-				int n;
-				while ((n = reader.read(buffer)) != -1) {
-					writer.write(buffer, 0, n);
-				}
-			} finally {
-				is.close();
+		Button button_submit = new Button(this.getApplicationContext());
+		button_submit.setText("Submit");
+		button_submit.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				Toast later = Toast.makeText(getApplicationContext(), "I'll do it later", Toast.LENGTH_LONG);
+				later.show();
+				//System.out.println(tex.getText());
 			}
-			return writer.toString();
-		} else {
-			return "";
-		}
-	} */
-
+		});
+		
+		Button button_cancel = new Button(this.getApplicationContext());
+		button_cancel.setText("Cancel");
+		button_cancel.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				Toast later = Toast.makeText(getApplicationContext(), "I'll cancel it later", Toast.LENGTH_LONG);
+				later.show();
+				//System.out.println(tex.getText());
+			}
+		});
+		
 	}
 }
